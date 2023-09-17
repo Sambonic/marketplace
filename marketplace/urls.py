33f74f4e785e8d4814bde4ce9a0e26a2f12fbdc7
Sambonic <124459402+Sambonic@users.noreply.github.com>
2023-09-18 02:22:21 +0300

    Updated settings

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
     path('',include('core.urls')),
@@ -9,4 +8,4 @@ urlpatterns = [
     path('dashboard/',include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
